509d5f6f82d1a0fa16fb35ce65fb07efaa3ce042
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-06-09 16:09:41 +0400

    step-forward to meme generator Added text inputs and synchronized them

--- meme generator/src/components/Meme.jsx
+++ meme generator/src/components/Meme.jsx
@@ -8,7 +8,10 @@ export default function Meme() {
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
     const [allMemeImages, setAllMemeImages] = React.useState(memesData)
-    
+    const [memeText, setMemeText] = React.useState({
+        top_txt: '',
+        bottom_txt: ''
+    })
     
     function getMemeImage() {
         const memesArray = allMemeImages.data.memes
@@ -20,16 +23,28 @@ export default function Meme() {
         }))
         
     }
+    function handleText(event) {
+        setMemeText(prev =>({
+            ...prev,
+            [event.target.name]: event.target.value
+    }))
+    }
     
     return (
         <main>
             <div className="form">
                 <input 
+                    name="top_txt"
+                    onChange={handleText}
+                    value={memeText.top_txt}
                     type="text"
                     placeholder="Top text"
                     className="form--input"
-                />
-                <input 
+                    />
+                <input
+                    name="bottom_txt"
+                    onChange={handleText}
+                    value={memeText.bottom_txt}
                     type="text"
                     placeholder="Bottom text"
                     className="form--input"
@@ -41,7 +56,11 @@ export default function Meme() {
                     Get a new meme image 🖼
                 </button>
             </div>
+            <div className="meme--container">
+                <h1 className="txt top-txt">{memeText.top_txt || "Type Text"}</h1>
             <img src={meme.randomImage} className="meme--image" />
+                <h1 className="txt bottom-txt">{memeText.bottom_txt || "Type Text"}</h1>
+            </div>
         </main>
     )
 }
