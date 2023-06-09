4cd2e2c3b86e52b96e9748aaed7b8876304bad4e
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-06-09 15:48:37 +0400

    added authorisation check

--- React_form/src/App.jsx
+++ React_form/src/App.jsx
@@ -2,34 +2,50 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [data,setData] = useState({})
+  const [data,setData] = useState({
+    name: "",
+    lname: "",
+    email: "",
+    okey: false,
+    status: "it"
+  })
   function synchronize(event) {
-    const curName = event.target.name;
-    const passData = event.target.type == "checkbox" ? event.target.checked : event.target.value
+    const curName = event.target.name; // Get name of the input
+    const passData = event.target.type == "checkbox" ? event.target.checked : event.target.value // get Data from input
     setData({
       ...data,
       [curName]: passData
-    })
+    }) // change data
+  }
+  function handleSubmit(event) { // log data
+    event.preventDefault();
+    let count = 0;
+    Object.keys(data).forEach(elem => {
+      if(data[elem] === "") {
+        count+=1;
+      }
+    });
     console.log(data);
+    console.log(count > 0 ? "Fill all blanks" : "Logged in")
   }
   return (
     <>
       <form>
         <label>Your name</label>
-        <input name='name' onChange={synchronize} value={data.name} type='text' />
+        <input name='name' onChange={synchronize} value={data.name} type='text' required/>
         <label>Your LName</label>
-        <input name='lname' onChange={synchronize} value={data.lname} type='text' />
+        <input name='lname' onChange={synchronize} value={data.lname} type='text' required/>
         <label>Your Email</label>
-        <input name='email' onChange={synchronize} type='email' />
+        <input name='email' onChange={synchronize} value={data.email} type='email' required />
         <label>Are you OK?</label>
-        <input name='okey' type='checkbox' onChange={synchronize} />
+        <input name='okey' type='checkbox' onChange={synchronize} defaultChecked required />
         <fieldset>
         <legend>Are you employed?</legend>
-        <input onChange={synchronize} name='status' value={'it'} type='radio'/><label>IT</label>
-        <input onChange={synchronize} name='status' value={'business'} type='radio'/><label>Business</label>
-        <input onChange={synchronize} name='status' value={'not_employed'} type='radio'/><label>Not-employed</label>
+        <input onChange={synchronize} name='status' value={'it'} type='radio' required defaultChecked /><label>IT</label>
+        <input onChange={synchronize} name='status' value={'business'} type='radio' required/><label>Business</label>
+        <input onChange={synchronize} name='status' value={'not_employed'} type='radio' required/><label>Not-employed</label>
         </fieldset>
-        <button>Submit</button>
+        <button onClick={handleSubmit}>Submit</button>
       </form>
     </>
   )
