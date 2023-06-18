004de18e5101d9c530fdff7adec20a6c80527d38
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-06-18 17:56:46 +0400

    Done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 import './App.css'
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false)
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar toggleDarkMode={() => setDarkMode(prev => !prev)} darkMode={darkMode}/>
+            <Main darkMode={darkMode} />
         </div>
     )
 }
